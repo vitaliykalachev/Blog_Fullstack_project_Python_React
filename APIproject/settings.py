659d84dc97bf7382499e0faca48ffffc31b2d9e1
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-11 18:32:04 +0700

    install bootstrap

--- APIproject/settings.py
+++ APIproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r8x%#qfv=bkcnt@8f99j68#l0nzz7ps)o$pxpuu2j*021*pwgj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'blog-python-react-django.herokuapp.com']
 
 
 # Application definition
