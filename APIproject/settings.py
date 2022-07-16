dcc2838ce164d8371fb02c97f68899ceb2c0220b
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-16 12:55:53 +0700

    comment static dirs

--- APIproject/settings.py
+++ APIproject/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-STATICFILES_DIRS = [BASE_DIR, "build"]
+# STATICFILES_DIRS = [BASE_DIR, "build"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
