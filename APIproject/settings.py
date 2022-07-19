00633c1438e2d208d108a05d8d67788d3facdf71
vitaliykalachev <vitaliy.kalachev@gmail.com>
2022-07-19 15:42:18 +0700

    cors_allowed

--- APIproject/settings.py
+++ APIproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ.get['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,7 +63,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
-
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'APIproject.urls'
